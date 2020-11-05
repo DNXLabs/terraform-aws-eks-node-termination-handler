3a377946ff930acc8e0631ec7bbe900043b007ef
Arthur Diniz <arthurbdiniz@gmail.com>
2020-11-04 23:08:32 -0300

    Clean required_providers
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- versions.tf
+++ versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = "~> 0.13.0"
+  required_version = "~> 0.13"
+
+  required_providers {
+    aws              = ">= 2.0, < 4.0"
+    helm             = ">= 1.0, < 1.4.0"
+    kubernetes       = ">= 1.10.0"
+  }
 }

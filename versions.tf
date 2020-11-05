7975200da18bdcde02dacc4c783ee4139ccb5dab
Arthur Diniz <arthurbdiniz@gmail.com>
2020-11-04 23:26:45 -0300

    Format versions.tf file
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 0.13"
 
   required_providers {
-    aws              = ">= 3.13, < 4.0"
-    helm             = ">= 1.0, < 1.4.0"
-    kubernetes       = ">= 1.10.0"
+    aws        = ">= 3.13, < 4.0"
+    helm       = ">= 1.0, < 1.4.0"
+    kubernetes = ">= 1.10.0"
   }
 }
